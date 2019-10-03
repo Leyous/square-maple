d050b1005d44cacc190c5e3423d1b421b02bf7f3
Stackbit <projects@stackbit.com>
2019-10-03 19:16:45 +0000

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+if [[ -z "${STACKBIT_API_KEY}" ]]; then
+    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
+else
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d9648dfe6b92900198d596f 
+fi
+jekyll build
+./inject-netlify-identity-widget.js _site
